f962eb21ac5c1c24703ade92af5ecbfad780ae95
ferrytsui <ferrytsui@163.com>
2021-12-30 00:02:58 +0800

    proxy的基本使用demo

--- 响应式原理/proxy_demo2.js
+++ 响应式原理/proxy_demo2.js
@@ -9,12 +9,18 @@ const obj = {
 const objProxy = new Proxy(obj, {
   //获取值时的捕获器
   get: function (target, key) {
+    console.log(`监听到对象的${key}属性被访问了`, target)
     return target[key]
   },
   //设置值时的捕获器
   set: function (target, key, newValue) {
+    console.log(`监听到对象的${key}属性被设置了`, target)
     target[key] = newValue
   }
 })
 
 console.log(objProxy.name)
+console.log(objProxy.age)
+
+objProxy.name = 'neymar'
+objProxy.age = 18
